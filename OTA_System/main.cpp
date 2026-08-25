f17415c32afe012927c991fe10295e0f60e285de
tlagkfla97-a11y <tlagkfla97@gmail.com>
2026-08-25 16:50:50 +0900

    폰트
    폰트추가

--- OTA_System/main.cpp
+++ OTA_System/main.cpp
@@ -1,10 +1,32 @@
 #include "ui/otamanager.h"
+#include <QFontDatabase>
+#include <QFont>
+#include <QCoreApplication>
+#include <QDebug>
 
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+     QString fontPath =
+        QCoreApplication::applicationDirPath()
+        + "/NotoSansCJK-Regular.ttc";
+
+    int fontId = QFontDatabase::addApplicationFont(fontPath);
+
+    if (fontId >= 0) {
+        QStringList families =
+            QFontDatabase::applicationFontFamilies(fontId);
+
+        if (!families.isEmpty()) {
+            a.setFont(QFont(families.first()));
+            qDebug() << "Loaded font:" << families.first();
+        }
+    } else {
+        qDebug() << "Failed to load font:" << fontPath;
+    }
     OtaManager w;
     w.show();
     return QApplication::exec();
